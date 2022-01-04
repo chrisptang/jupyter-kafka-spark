32f2eebc9ac8a0bb16125edc5dc51c3350731695
Tang Peng <derrick.tang@miniso.com>
2022-01-04 14:13:49 +0800

    using dynamic container name

--- custom-superset/init_superset.sh
+++ custom-superset/init_superset.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+export container_name=`docker ps |grep "apache/superset"|awk '{print $12}'`
+
 export USER_NAME="admin"
 export USER_FIRST_NAME="Admin"
 export USER_LAST_NAME="Superset"
 export USER_EMAIL="admin@fruits.co"
 export USER_PASSWORD="ptang@220104"
 
-docker exec -it superset-prod superset fab create-admin \
+docker exec -it ${container_name} superset fab create-admin \
                --username ${USER_NAME} \
                --firstname ${USER_FIRST_NAME} \
                --lastname ${USER_LAST_NAME} \
                --email ${USER_EMAIL} \
                --password ${USER_PASSWORD} \
-&& docker exec -it superset-prod superset db upgrade \
-&& docker exec -it superset-prod superset init
+&& docker exec -it ${container_name} superset db upgrade \
+&& docker exec -it ${container_name} superset init

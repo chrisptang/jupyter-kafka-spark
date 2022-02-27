e986be3f7dd33745804772c7de612bc60f05f722
chrisptang <chris.p.tang@gmail.com>
2022-02-27 16:32:30 +0800

    adding zeppelin notebooks:shopee-data

--- custom-zeppelin/zeppelin-env.sh
+++ custom-zeppelin/zeppelin-env.sh
@@ -1,4 +1,5 @@
 export ZEPPELIN_PORT=8280
 export ZEPPELIN_ADDR=0.0.0.0
 export SPARK_HOME=/opt/spark
-export PYSPARK_PYTHON=`which python`
+export ZEPPELIN_NOTEBOOK_DIR=/opt/zeppelin/notebook
+export ZEPPELIN_LOG_DIR=/logs

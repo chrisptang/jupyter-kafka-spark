69e1e13317ae6fb9fa819185b8d5f741b42768a6
Tang Peng <derrick.tang@miniso.com>
2022-02-28 14:55:04 +0800

    custom spark

--- custom-spark-standalone/start-spark.sh
+++ custom-spark-standalone/start-spark.sh
@@ -5,14 +5,17 @@
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
 
-export SPARK_MASTER_HOST=`hostname`
+    export SPARK_MASTER_HOST=`hostname`
+    echo "SPARK_MASTER_HOST:${SPARK_MASTER_HOST}" >> $SPARK_MASTER_LOG
 
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
+    cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip 0.0.0.0 \
+    --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
 
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT --port $SPARK_WORKER_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+    cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip 0.0.0.0 --webui-port $SPARK_WORKER_WEBUI_PORT \
+    --port $SPARK_WORKER_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "submit" ];
 then

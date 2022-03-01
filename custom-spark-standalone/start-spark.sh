37e5965212e103a0bc6fa984aa086474640994f4
Tang Peng <derrick.tang@miniso.com>
2022-03-01 11:22:25 +0800

    adding spark.blockManager.port to conf

--- custom-spark-standalone/start-spark.sh
+++ custom-spark-standalone/start-spark.sh
@@ -4,19 +4,27 @@
 
 export SPARK_HOST=`hostname`
 
+BLOCK_MANAGER_PORT=${SPARK_BLOCK_MANAGER_PORT=39191}
+
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
-    echo "SPARK_MASTER_HOST:${SPARK_MASTER_HOST}" >> $SPARK_MASTER_LOG
+    echo "SPARK_HOST:${SPARK_HOST}" >> $SPARK_MASTER_LOG
 
     cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip ${SPARK_HOST} \
+    --conf spark.blockManager.port=${BLOCK_MANAGER_PORT} \
     --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
+
+    ps aux|grep java >> $SPARK_MASTER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
 
     cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip ${SPARK_HOST} \
     --webui-port $SPARK_WORKER_WEBUI_PORT \
+    --conf spark.blockManager.port=${BLOCK_MANAGER_PORT} \
     --port $SPARK_WORKER_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+
+    ps aux|grep java >> $SPARK_WORKER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "submit" ];
 then
--- custom-zeppelin/start_zeppelin_container.sh
+++ custom-zeppelin/start_zeppelin_container.sh
@@ -7,8 +7,9 @@
 # && rm apache-spark.tgz
 
 # forward spark.driver.port=47077
+# forward spark.blockManager.port=39191
 
-docker run -d -u $(id -u) -p 8280:8080 -p 47077:47077 -v $PWD/zeppelin-notebook:/notebook \
+docker run -d -u $(id -u) -p 8280:8080 -p 47077:47077 -p 39191:39191 -v $PWD/zeppelin-notebook:/notebook \
 -v $PWD/zeppelin-logs:/logs -v $PWD/log-received:/log-received -v $PWD/spark/:/opt/spark \
 -e SPARK_HOME=/opt/spark -e ZEPPELIN_NOTEBOOK_DIR='/notebook' -e ZEPPELIN_LOG_DIR='/logs' \
 -v $PWD/zeppelin-warehouse:/warehouse --name zeppelin apache/zeppelin:0.10.0
